402026032b840d62838f44549b8d9eb155471538
berrysitalj <berry_si_ta_lj@yahoo.co.jp>
2021-04-22 21:14:12 +0900

    Update application

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -14,7 +14,7 @@ end
 
 	def self.search(search)
       return Shop.all unless search
-      Shop.where(['yomi LIKE ? or addres1 LIKE ? or addres2 LIKE ? or shopname LIKE ? or station LIKE ? or area2 LIKE ? ', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
+      Shop.where(['yomi LIKE ? or addres1 LIKE ? or addres2 LIKE ? or shopname LIKE ? or station LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
     end
 
 end
