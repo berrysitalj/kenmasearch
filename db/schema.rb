69efa5c374a044d0c764749d7d9355fbd477688b
berrysitalj <berry_si_ta_lj@yahoo.co.jp>
2021-04-27 22:04:17 +0900

    Update application

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_123941) do
+ActiveRecord::Schema.define(version: 2021_04_27_130252) do
 
   create_table "owners", force: :cascade do |t|
     t.string "name"
     t.string "pass"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "senders", force: :cascade do |t|
+    t.integer "category"
+    t.string "shopname"
+    t.string "yomi"
+    t.string "tel1"
+    t.string "tel2"
+    t.string "tel3"
+    t.string "area"
+    t.string "addres"
+    t.string "charge"
+    t.string "tel4"
+    t.string "tel5"
+    t.string "tel6"
+    t.string "chargemail"
+    t.string "question"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
