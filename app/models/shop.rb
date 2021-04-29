21353fc679ca83c95cf93ff131a9f1017457ba13
berrysitalj <berry_si_ta_lj@yahoo.co.jp>
2021-04-29 10:05:15 +0900

    Update shop.rb

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,8 @@
 class Shop < ApplicationRecord
+  validates :pass, {presence: true}
+
+	def self.search(search)
+      return Shop.all unless search
+      Shop.where(['addres1 LIKE ? or addres2 LIKE ? or shopname LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"])
+    end
 end
