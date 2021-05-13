e5f4a97fb5e13a4f4a120f3afa515bb2383cdff5
berrysitalj <berry_si_ta_lj@yahoo.co.jp>
2021-05-13 10:48:27 +0900

    Update application

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,5 @@
 class Shop < ApplicationRecord
-<<<<<<< HEAD
+
 
 	with_options presence: true do
 		validates :userid
@@ -13,12 +13,12 @@ class Shop < ApplicationRecord
 		validates :shopname
 	end
 
-	def self.search(search)
-=======
+	
+
   validates :pass, {presence: true}
 
     def self.search(search)
->>>>>>> b54174fbbcd6a930f3e6250351e1fe605a15ade0
+
       return Shop.all unless search
       Shop.where(['addres1 LIKE ? or addres2 LIKE ? or shopname LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"])
     end
