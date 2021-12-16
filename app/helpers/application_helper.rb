d3356b4261a91e39eb9d2c056903a756c0d97d76
berrysitalj <berry_si_ta_lj@yahoo.co.jp>
2021-12-16 18:53:48 +0900

    Update application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 module ApplicationHelper
+  require "uri"
+
+  def text_url_to_link text
+
+    URI.extract(text, ['http','https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+      text.gsub!(url, sub_text)
+    end
+
+    return text
+  end
 
 	def embedded_svg filename, options={}
     file = File.read(Rails.root.join('app', 'assets', 'images', filename))
@@ -8,6 +21,5 @@ module ApplicationHelper
       svg['class'] = options[:class]
     end
     doc.to_html.html_safe
-end
-
+  end
 end
