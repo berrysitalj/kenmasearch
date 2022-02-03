f1959f3f694a48daba592b30dff01100f47d0da1
berrysitalj <berry_si_ta_lj@yahoo.co.jp>
2022-02-03 16:22:06 +0900

    Update application

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,11 +1,9 @@
 class ShopsController < ApplicationController
   protect_from_forgery
-  before_action :owner_logged_in?, only: [:index, :new, :create, :destroy]
-  # before_action :user_logged_in?, only: [:edit, :update]
+  before_action :owner_logged_in?, only: [:index, :new, :create, :edit, :update, :destroy]
   skip_before_action :user_logged_in?
 
   def index
-  	@shops = Shop.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show
@@ -21,54 +19,17 @@ class ShopsController < ApplicationController
   end
 
   def new
-  	@shop = Shop.new
   end
 
   def create
-  	@shop = Shop.new(userid: params[:userid])
-    @shop.pass = params[:pass]
-    @shop.shopname = params[:shopname]
-    @shop.shoptel = params[:shoptel]
-    @shop.addres1 = params[:addres1]
-    @shop.addres2 = params[:addres2]
-    @shop.area = params[:area]
-    @shop.station = params[:station]
-    @shop.topphoto = params[:topphoto]
-    @shop.freeicon1 = params[:freeicon1]
-    @shop.freeicon2 = params[:freeicon2]
-    @shop.freeicon11 = params[:freeicon11]
-    @shop.freeicon12 = params[:freeicon12]
-    @shop.freeicon13 = params[:freeicon13]
-    @shop.freeicon14 = params[:freeicon14]
-    if @shop.save
-      flash[:notice] = "新規登録が完了しました"
-      redirect_to("/shops/index")
-     else
-       render("shops/new")
-    end
   end
 
   def edit
-  	@shop = Shop.find_by(userid: params[:id])
   end
   
   def update
-  	@shop = Shop.find_by(userid: params[:id])
-  	@shop.shopname = params[:shopname]
-    
-
-    
-    if @shop.save
-      redirect_to("/user_home")
-    else
-      render("shops/edit")
-    end
   end
 
   def destroy
-    @shop = Shop.find_by(id: params[:id])
-    @shop.destroy
-    flash[:notice] = "投稿を削除しました"
-    redirect_to("/shops/index")
   end
 end
